15c4e349caef9ed2185957dd47f6b33c02de20e0
febbyfitri <febbyftrp@student.telkomuniversity.ac.id>
2020-12-18 23:16:27 +0700

    hello world iseng!

--- app/src/main/java/mmu/edu/my/newproject/MainActivity.java
+++ app/src/main/java/mmu/edu/my/newproject/MainActivity.java
@@ -2,7 +2,10 @@ package mmu.edu.my.newproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new AlertDialog.Builder(MainActivity.this)
+                .setTitle("Alert")
+                .setMessage("This is an alert (I Deo Kelvin Aprianta)")
+                .setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "nothing", Toast.LENGTH_SHORT).show();
+                    }
+                }).show();
     }
 }
